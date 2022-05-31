5430692e3351703ba54a3e120cd4b3b1c9db4caf
TommyHacker <t.hacker.webdev@gmail.com>
2022-05-31 10:07:07 +0100

    review branch created

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # search-api
+
+<i>this is a simple search api which utilises an express backend which responds with html using server side rendering
+
+<b>meaning</b> the data rendered on the page is sent from the express back end to a template html/ejs file to fill in the gaps</i>
+
+# setup
+
+<code>npm install</code>
+
+<code>npm run dev <i>or</i> npm run start</code>
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	try {
+		console.log('home route gotten.');
+		res.render('index');
+	} catch (err) {
+		console.log(err.message);
+		res.send('something went wrong, try again.');
+	}
+});
+
+
+
+module.exports = app;
